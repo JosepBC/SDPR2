fc1357a3fa595815705a685296b9588b32764318
Josep Bello Curto <bellocontacto@gmail.com>
2021-05-21 12:54:13 +0200

    Diferenced wuery from covid vaccine

--- twitterCrawler.py
+++ twitterCrawler.py
@@ -12,6 +12,9 @@ if __name__ == "__main__":
     maxTweets = 1000000
     count = 0
 
+    if(len(sys.argv) == 1):
+        print("Format: twitterCrawler.py vaccine query max_id")
+
     if(len(sys.argv) < 2):
         print("Error, you must specify the vaccine")
         exit
@@ -19,23 +22,31 @@ if __name__ == "__main__":
     vaccine = sys.argv[1]
     print("Vaccine: "+str(vaccine))
     
+    if(len(sys.argv) < 3):
+        query = vaccine
+    else:
+        query = sys.argv[2]
+
+    print("Query: "+query)
+
     load_dotenv()
     auth = tweepy.OAuthHandler(os.environ.get("consumer_key"), os.environ.get("consumer_secret"))
     auth.set_access_token(os.environ.get("access_token"), os.environ.get("access_secret"))
 
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, parser=JSONParser(), retry_count=5, retry_delay=5)
 
-    if(len(sys.argv) > 2):
-        maxID = sys.argv[2]
+    if(len(sys.argv) > 3):
+        maxID = sys.argv[3]
         print("Using max_id: "+str(maxID))
 
     while tweetCount < maxTweets:
         if(maxId <= 0):
-            newTweets = api.search(q=vaccine, lang="es", count=tweetsPerQry, result_type="mixed", tweet_mode="extended")
+            newTweets = api.search(q=query, lang="es", count=tweetsPerQry, result_type="mixed", tweet_mode="extended")
         else:
-            newTweets = api.search(q=vaccine, lang="es", count=tweetsPerQry, max_id=str(maxId - 1), result_type="mixed", tweet_mode="extended")
+            newTweets = api.search(q=query, lang="es", count=tweetsPerQry, max_id=str(maxId - 1), result_type="mixed", tweet_mode="extended")
 
-        if not newTweets:
+        if(len(newTweets["statuses"]) == 0):
+            print("No more tweets")
             break
         
         for tweet in newTweets["statuses"]:
